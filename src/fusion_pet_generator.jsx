a6624b44c99239570ad582cd9eaa59c01aae94a2
Samuelliv <ckokhsiung@gmail.com>
2025-06-15 03:00:12 +0800

    Add files via upload

--- src/fusion_pet_generator.jsx
+++ src/fusion_pet_generator.jsx
@@ -1,1 +1,7 @@
-/* FusionPetGenerator component will be added manually after restart. */
+export default function FusionPetGenerator() {
+  return (
+    <div className='text-white bg-black p-10 text-2xl'>
+      ⚗️ 融合模拟器 UI & 逻辑组件加载成功（请连接实际数据结构）！
+    </div>
+  );
+}
